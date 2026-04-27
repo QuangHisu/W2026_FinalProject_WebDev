af02b454aba32f385e531c589ed4d6a7d93477db
Tran Quang Hieu Nguyen <quanghieu22092005@gmail.com>
2026-04-27 17:58:18 -0400

    UI Update

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/orders/edit', [OrderController::class, 'edit'])->name('orders.edit');
 Route::put('/orders/update', [OrderController::class, 'update'])->name('orders.update');
